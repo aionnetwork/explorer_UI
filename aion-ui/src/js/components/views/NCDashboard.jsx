bb5078f8238bdcd243d6558d4eba0475a0e7751e
Kenroy <kenroy@aion.network>
2018-07-24 16:12:52 -0400

    ui/ux update 1.0-View all added

--- aion-ui/src/js/components/views/NCDashboard.jsx
+++ aion-ui/src/js/components/views/NCDashboard.jsx
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Tabs2, Tab2 } from "@blueprintjs/core";
+import {Button, Tabs2, Tab2 } from "@blueprintjs/core";
 
 import NCRecentBlocks from 'components/dashboard/NCRecentBlocks';
 import NCDashboardKPIs from 'components/dashboard/NCDashboardKPIs';
@@ -30,12 +30,22 @@ export default class NCDashboard extends Component
             <div className="NCRecentBlocksContainer">
               <div className="NCTitle section blue">
                 <span className="title">Recent Blocks</span>
+                <Button 
+                  className=" pt-button pt-minimal"
+                  
+                  onClick={() => {hashHistory.push('/blocks');}}
+                  text="View All"/>
               </div>
               <NCRecentBlocks/>
             </div>
             <div className="NCRecentTransactionsContainer">
               <div className="NCTitle section">
                 <span className="title">Recent Transactions</span>
+                <Button 
+                  className=" pt-button pt-minimal"
+                  
+                  onClick={() => {hashHistory.push('/transactions');}}
+                  text="View All"/> 
               </div>
               <NCTxnRealtime/>
             </div>
