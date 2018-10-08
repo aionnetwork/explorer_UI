d851bed2bcfaf48971421dfd8c333a6b7e5bc855
Kenroy White <kenroy@aion.network>
2018-10-07 20:26:43 -0400

    recaptcha frontend download updated

--- aion-ui/src/js/components/views/NCDwnRetrieve.jsx
+++ aion-ui/src/js/components/views/NCDwnRetrieve.jsx
@@ -6,7 +6,7 @@ import moment from 'moment';
 
 import Recaptcha from 'react-recaptcha';
 
-import { Position, Popover, Tab2, Tabs2, Tooltip, Button, Menu, MenuItem, PopoverInteractionKind } from "@blueprintjs/core";
+import { Position, Intent, Popover, Tab2, Tabs2, Tooltip, Button, Menu, MenuItem, PopoverInteractionKind } from "@blueprintjs/core";
 import { Select } from "@blueprintjs/select";
 
 import NCBlkTable from 'components/blocks/NCBlkTable';
@@ -35,8 +35,11 @@ class NCDwnRetrieve extends Component
     this.state = {
       isFetching: false,
       queryStr: '',
-      entity: NCEntity.ACCOUNT
+      entity: NCEntity.ACCOUNT,
+      recaptcha:'',
+      button: true
     } 
+    this.captcha = this.captcha.bind(this);
   }
 
   componentWillMount() {
@@ -58,27 +61,44 @@ class NCDwnRetrieve extends Component
 
   requestDownload = () => {
        //this.download('uyuy');
-       network.getAccRetrieveCSV(this.props.params.accId, 'nothing');
+       //network.getAccRetrieveCSV(this.props.params.accId, 'nothing');
       
   }
 
-  download = (a) => {
+  captcha(key){
+      this.setState({recaptcha:response})
+  }
+
+  download(a){
        console.log(a);
+       this.setState({button:true});
        network.getAccRetrieveCSV(this.props.params.accId, a);
        //return;
   }
 
+  verifyCallback(response) {
+       //this.state.recaptcha = response;
+        this.setState({recaptcha:response,button:false});
+   };
+
+ 
   render() {
     
-    const verifyCallback = function (response) {
-      console.log(response);
-      return this.download(response);
-    };
+    
     const style = {position:'relative',textAlign:'center',width:'100%',margin:'auto',top:'100px',padding:'25px' };
     const recapcontainer = {position:'relative',width:'310px',margin:'auto',top:'100px',padding:'5px' };
     const stylerecaptcha = {margin:'auto', };
 
+    // create a variable to store the component instance
+    let recaptchaInstance;
+ 
+    // create a reset function
+    const resetRecaptcha = () => {
+      recaptchaInstance.reset();  
+    };
 
+    let button = this.state.button;
+    let recaptcha= this.state.recaptcha;
 
     return (
       <div className= {''} style = {style}>
@@ -87,10 +107,13 @@ class NCDwnRetrieve extends Component
        
        <div className= {''} style = {recapcontainer}>
         <Recaptcha
+          ref={e => recaptchaInstance = e}
           style = {stylerecaptcha}
           sitekey="6LfwrXMUAAAAAEpZCdMFD0ba96ryOUDGPMyqHZPA"
-          verifyCallback={verifyCallback}
+          verifyCallback={this.verifyCallback.bind(this)}
         />
+        <br/>
+        <Button  text='Download' disabled={button} onClick={() => {resetRecaptcha(); this.download(recaptcha)}} className = "pt-button pt-minimal" rightIconName="download" />
         
        </div>
         
