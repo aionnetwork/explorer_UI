b912c22e49afebfb181a7daed8968a70bda98f26
Kenroy White <kenroy@aion.network>
2019-02-15 14:26:19 -0400

    updated value display in internal transfers table

--- aion-ui/src/js/components/transactions/NCTxnTableOwnTransfer.jsx
+++ aion-ui/src/js/components/transactions/NCTxnTableOwnTransfer.jsx
@@ -124,7 +124,7 @@ export default class NCTxnTableOwnTransfer extends Component
         fromAddr = entity.fromAddr;
         toAddr = entity.toAddr;
         transferTimestamp = entity.blockTimestamp;
-        value = entity.value;//BigNumber(String(entity.valueTransferred), 16).toString(10);//entity.valueTransferred;
+        value = (entity.value) ? nc_decimalPrettify(entity.value) : nc_decimalPrettify(entity.valueTransferred);//BigNumber(String(entity.valueTransferred), 16).toString(10);//entity.valueTransferred;
         transactionHash = entity.transactionHash;//let bal = nc_numFormatterACSensitive(entity.balance);
         
       }
