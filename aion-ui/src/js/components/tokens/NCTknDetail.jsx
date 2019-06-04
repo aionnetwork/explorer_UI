f3decf7721d4c98453e4ccf9c2b11d7ea10c53b6
Kenroy White <kenroy@aion.network>
2019-06-04 15:26:53 -0400

    updated token to account for negative total/liquid supply

--- aion-ui/src/js/components/tokens/NCTknDetail.jsx
+++ aion-ui/src/js/components/tokens/NCTknDetail.jsx
@@ -109,11 +109,11 @@ export default class NCTknDetail extends Component
       
       {
         field: MSG.strings.Tkn_detail_row7,
-        value: nc_decimalPrettify(nc_addDecimal(entity.totalSupply.toString(),entity.tokenDecimal,8))+ " " + entity.symbol,
+        value: (entity.totalSupply)? entity.totalSupply : nc_decimalPrettify(nc_addDecimal(entity.totalSupply.toString(),entity.tokenDecimal,8))+ " " + entity.symbol,
       },
       {
         field: MSG.strings.Tkn_detail_row8,
-        value: nc_decimalPrettify(nc_addDecimal(entity.liquidSupply.toString(),entity.tokenDecimal,8))+ " "+ entity.symbol,
+        value: (entity.liquidSupply)? entity.liquidSupply : nc_decimalPrettify(nc_addDecimal(entity.liquidSupply.toString(),entity.tokenDecimal,8))+ " "+ entity.symbol,
       },
      
       
